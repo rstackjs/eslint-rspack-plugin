6531af90b68b3af3cb71c0a0d66f6eca364a952b
neverland <jait.chenjiahan@gmail.com>
2026-05-11 19:09:06 +0800

    chore: migrate build to Rslib (#40)

---
+++ rslib.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    {
+      syntax: 'es2023',
+      dts: true,
+    },
+  ],
+});
--- types/ESLintError.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ESLintError extends Error {
-  /**
-   * @param {string=} messages
-   */
-  constructor(messages?: string | undefined);
-  stack: string;
-}
